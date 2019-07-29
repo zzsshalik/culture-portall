b7eff3b6c805a2784a15e9fb17615a67be8589d6
Dasha <lomakodasha@gmail.com>
2019-07-29 23:59:31 +0300

    styles: add styles for table

--- src/components/authorPageComponents/ListOfArts/ListOfArts.js
+++ src/components/authorPageComponents/ListOfArts/ListOfArts.js
@@ -1,22 +1,22 @@
 import React from 'react'
 
-import style from './ListOfArts.module.scss';
+import artsListStyle from './ListOfArts.module.scss';
 
 class ListOfArts extends React.Component {
     render() {
         return(
             <>
-            <h3>Artist works</h3>
+            <h3 className={artsListStyle.artsList_title}>Artist works</h3>
             <table>
                 <tr>
-                    <th>Date of creation</th>
-                    <th>Art</th>
+                    <th className={artsListStyle.artsList_th}>Date of creation</th>
+                    <th className={artsListStyle.artsList_th}>Art</th>
                 </tr>
                 {this.props.artsArrayOfObject.map((edge) => {
                     return (
                         <tr>
-                            <td>{edge.time}</td>
-                            <td>{edge.art}</td>
+                            <td className={artsListStyle.artsList_td_time}>{edge.time}</td>
+                            <td className={artsListStyle.artsList_td}>{edge.art}</td>
                         </tr>
                     )
                 })}
