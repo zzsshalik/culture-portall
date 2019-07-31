c0b51b9dc50a6250692b2f8ff760a15eebb2158a
Dasha <lomakodasha@gmail.com>
2019-07-31 18:33:22 +0300

    refactor: add width and height to pictures in gallery

--- src/components/authorPageComponents/Gallery/Gallery.js
+++ src/components/authorPageComponents/Gallery/Gallery.js
@@ -13,10 +13,13 @@ class Gallery extends React.Component {
                 <h3 className={galleryStyles.gallery_title}>Gallery</h3>
                 {this.props.photosArrayOfObjects.map((edge) => {
                     const imageInfo = {};
+                    const delimiter = edge.description.search('x'); 
+                    const sizeLength = edge.description.length;
+
                     imageInfo.src = edge.file.url;
                     imageInfo.thumbnail = edge.file.url;
-                    imageInfo.thumbnailWidth = 320;
-                    imageInfo.thumbnailHeight = 150;
+                    imageInfo.thumbnailWidth = edge.description.substring(0,delimiter);
+                    imageInfo.thumbnailHeight = edge.description.substring(delimiter + 1,sizeLength);
                     imagesArray.push(imageInfo);
                 })}
 
